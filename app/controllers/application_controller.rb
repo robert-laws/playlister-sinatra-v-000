f52b8c424beba5df6bedfb2c053110ae5a3a27af
Robert Laws <roblaws@me.com>
2017-11-03 09:42:45 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -40,8 +40,6 @@ class ApplicationController < Sinatra::Base
     @song.genre_ids = params[:genres]
     @song.save
 
-    flash[:message] = "Successfully created song."
-
     redirect("/songs/#{@song.slug}")
   end
 
@@ -56,8 +54,6 @@ class ApplicationController < Sinatra::Base
     @song.update(params[:song])
     @song.artist = Artist.find_or_create_by(name: params[:artist][:name])
     @song.save
-
-    flash[:message] = "Successfully updated song."
     redirect("/songs/#{@song.slug}")
   end
 
