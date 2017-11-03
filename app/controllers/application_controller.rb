d055d1f6f73c17ef1329091bbf2a0ee20301fc97
Robert Laws <roblaws@me.com>
2017-11-03 09:24:15 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,13 +7,13 @@ class ApplicationController < Sinatra::Base
     erb :index
   end
 
-  get '/artists' do
-    @artists = Artist.all
-    erb :'artists/index'
-  end
-
-  get '/artists/:slug' do
-    @artist = Artist.find_by_slug(params[:slug])
-    erb :'artists/show'
-  end
+  # get '/artists' do
+  #   @artists = Artist.all
+  #   erb :'artists/index'
+  # end
+  # 
+  # get '/artists/:slug' do
+  #   @artist = Artist.find_by_slug(params[:slug])
+  #   erb :'artists/show'
+  # end
 end
